c35af205c4a28ab4d02b892f4a35e4c2060bdeed
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-01-30 22:16:24 -0300

    Frist commit

---
+++ src/components/login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { GoogleLogin } from '@react-oauth/google';
+import { useNavigate } from 'react-router-dom';
+import { FcGoogle } from 'react-icons/fc';
+
+export default function Login() {
+  return (
+    <div>Login</div>
+  )
+}
