2324d57b26d59cdd9f532e20511374e8bff8b2f3
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-02-01 12:48:12 -0300

    Added Sidebar

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from 'react';
+import { useState, useRef, useEffect } from 'react';
+import { HiMenu, HiTrendingUp } from 'react-icons/hi';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { Link, Route, Routes } from 'react-router-dom';
+
+import { Sidebar, UserProfile } from '../components/index.js';
+import Pins from './Pins.jsx';
+import { client } from '../client.js'
+import { userQuery } from '../utils/data.js'
+import logo from '../assets/logo.png'
 
 export default function Home() {
+
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+  const [user, setUser] = useState(null);
+  const userInfo = localStorage.getItem('user') !== undefined ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
+  useEffect(() => {
+    const query = userQuery(userInfo?.aud);
+
+    client.fetch(query)
+      .then((data) => {
+        setUser(data[0]);
+      })
+  },[])
   return (
-    <>
-    <h1>Foi</h1>
-    </>
+    <div className='flex bg-gray-50 md:flex-col h-screen trasaction-height duration-75 ease-out'>
+      <div className='hidden md:flex h-sceen flex-initial'>
+        <Sidebar 
+          user={user && user}
+        />
+      </div>
+      <div className='flex md:hidden flex-row'>
+        <HiMenu fontSize={40} className='cursor-pointer' onClick={() => setToggleSidebar(HiTrendingUp)} />
+        <Link to='/'>
+          <img src={logo} alt="logo" className='w-28' />
+        </Link>
+        <Link to={`user-profile/${user?._id}`}>
+          <img src={user?.image} alt="user-Image" className='w-28' />
+        </Link>
+      </div>
+       {toggleSidebar && (
+        <div className='fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in'>
+          <div className='absolute w-full flex justify-end items-center p-2'>
+            <AiFillCloseCircle fontSize={30} className='cursor-pointer' onClick={() => setToggleSidebar(false)} />
+          </div>
+          <Sidebar 
+            user={user && user}
+            closseToggle={() => setToggleSidebar(false)}
+          />
+        </div>
+       )}
+    </div>
   )
 }
