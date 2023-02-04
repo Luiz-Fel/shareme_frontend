11088b3830e434b337b265c3875c718cfb5f67be
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-02-04 15:57:41 -0300

    Updated mobile header

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -15,6 +15,7 @@ export default function Home() {
   const [toggleSidebar, setToggleSidebar] = useState(false);
   const [user, setUser] = useState(null);
   const userInfo = localStorage.getItem('user') !== undefined ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+  const scrollRef = useRef(null);
 
   useEffect(() => {
     const query = userQuery(userInfo?.aud);
@@ -24,22 +25,29 @@ export default function Home() {
         setUser(data[0]);
       })
   },[])
+
+  useEffect(() => {
+    scrollRef.current.scrollTo(0, 0);
+  }, [])
+
+
   return (
-    <div className='flex bg-gray-50 md:flex-col h-screen trasaction-height duration-75 ease-out'>
+    <div className='flex bg-gray-50 md:flex-row flex-col h-screen trasaction-height duration-75 ease-out'>
       <div className='hidden md:flex h-sceen flex-initial'>
         <Sidebar 
           user={user && user}
         />
       </div>
       <div className='flex md:hidden flex-row'>
-        <HiMenu fontSize={40} className='cursor-pointer' onClick={() => setToggleSidebar(HiTrendingUp)} />
-        <Link to='/'>
-          <img src={logo} alt="logo" className='w-28' />
-        </Link>
-        <Link to={`user-profile/${user?._id}`}>
-          <img src={user?.image} alt="user-Image" className='w-28' />
-        </Link>
-      </div>
+        <div className='p-2 w-full flex flex-row justify-between items-center shadow-md'>
+          <HiMenu fontSize={40} className='cursor-pointer' onClick={() => setToggleSidebar(HiTrendingUp)} />
+          <Link to='/'>
+            <img src={logo} alt="logo" className='w-28' />
+          </Link>
+          <Link to={`user-profile/${user?._id}`}>
+            <img src={user?.image} alt="user-Image" className='w-28' />
+          </Link>
+        </div>
        {toggleSidebar && (
         <div className='fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in'>
           <div className='absolute w-full flex justify-end items-center p-2'>
@@ -51,6 +59,13 @@ export default function Home() {
           />
         </div>
        )}
+      </div>
+       <div className='pb-2 flex-1 h-screen overflow-y-scroll' ref={scrollRef}>
+          <Routes>
+            <Route path='/user-profile/:userId' element={<UserProfile />} />
+            <Route path='/*' element={<Pins user={user && user} />} />
+          </Routes>
+       </div>
     </div>
   )
 }
