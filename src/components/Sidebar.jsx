b6e0b10305ead8a37b627d492f69a3b3544cdc45
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-02-04 19:57:33 -0300

    Created Navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ import { RiHomeFill } from 'react-icons/ri'
 import logo from '../assets/logo.png'
 
 const notActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
-const activeStyle = 'flex items-center px-5 gap-3 font-extrabold border-black transition-all duration-200 ease-in-out capitalize';
+const activeStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize';
 
 const categories = [
   {name: 'animals' },
