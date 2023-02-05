087c6cbd69a8a6b653ed2d4d3015aa963eb852a1
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-02-05 11:44:11 -0300

    Created Spinner

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import { TailSpin } from  'react-loader-spinner'
+
+function Spinner({ message }) {
+  return (
+    <div className='flex flex-col justify-center items-center w-full h-full'>
+            <TailSpin
+                height='80'
+                width='80'
+                color = '#00BFFF'
+                ariaLabel='tail-spin-loading'
+                radius='1'
+                wrapperStyle={{}}
+                wrapperClass='m-5'
+                visible={true}
+            />
+
+            <p className='text-lg text-center px-2'>{message}</p>
+    </div>
+  )
+}
+
+export default Spinner
