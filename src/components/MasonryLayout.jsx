efdc64dc3241cadd787245dceebcbf930e2bbe4c
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-02-08 12:26:07 -0300

    Removed console.log

--- src/components/MasonryLayout.jsx
+++ src/components/MasonryLayout.jsx
@@ -13,7 +13,6 @@ const breakpointObj = {
 }
 
 function MasonryLayout({ pins }) {
-    console.log(pins)
   return (
     <Masonry className='flex animate-slide-fwd' breakpointCols={breakpointObj}>
         {pins?.map((pin) => <Pin key={pin._id} pin={pin} className='w-max' />)}
