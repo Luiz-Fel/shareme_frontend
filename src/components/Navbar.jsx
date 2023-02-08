82057acee7dc5be6fe0c0ee97900fda69c1eb6f7
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-02-08 11:00:33 -0300

    Corrected link redirection

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ function Navbar({ searchTerm, setSearchTerm, user }) {
         <Link to={`user-profile/${user?._id}`} className='hidden md:block'>
           {user && <img src={user.image} alt="user" className='w-14 h-12 rounded-lg' />}
         </Link>
-        <Link to={`user-profile/${user?._id}`} className='bg-black text-white rounded-lg w-12 h-12 md:w-14 md:h-12 flex justify-center items-center'>
+        <Link to={`create-pin`} className='bg-black text-white rounded-lg w-12 h-12 md:w-14 md:h-12 flex justify-center items-center'>
           <IoMdAdd />
         </Link>
       </div>
