863e444464056815fcfbdbfe98c427e1056e348d
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-03-15 01:27:28 -0300

    Changed icon

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
-import { RiHomeFill } from 'react-icons/ri'
+import { RiHome2Line } from 'react-icons/ri'
 
 import logo from '../assets/logo.png'
 import { categories } from '../utils/data';
 
-const notActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
-const activeStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize';
+const notActiveStyle = 'flex items-center px-5 gap-2 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
+const activeStyle = 'flex items-center px-5 gap-2 font-bold border-r-2 border-black transition-all duration-200 ease-in-out capitalize';
 
 
 function Sidebar({ user, closeToggle }) {
@@ -33,7 +33,7 @@ function Sidebar({ user, closeToggle }) {
             className={({ isActive }) => isActive ? activeStyle : notActiveStyle}
             onClick={handleCloseSidebar}
           >
-            <RiHomeFill />
+            <RiHome2Line />
             <span>Home</span>
           </NavLink>
           <h3 className='pl-5'>Discover categories</h3>
