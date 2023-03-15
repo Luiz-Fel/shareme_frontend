890e243771e6cffb8b101fb29f76713cd22a12b8
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-03-14 23:55:21 -0300

    Removed unused buttons

--- src/components/Pin.jsx
+++ src/components/Pin.jsx
@@ -78,62 +78,18 @@ function Pin({
               className="absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2"
               style={{ height: "100%" }}
             >
-              <div className="flex items-center justify-between">
-                <div className="flex gap-2">
-                  <a
-                    className="bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark tex-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none "
-                    href={`${image?.asset?.url}?dl=`}
-                    download
-                    onClick={(e) => e.stopPropagation()}
-                  >
-                    <MdDownloadForOffline className="" />
-                  </a>
-                </div>
-                {alreadySaved ? (
-                  <button
-                    type="button"
-                    className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none"
-                  >
-                    {save?.length} Saved
-                  </button>
-                ) : (
-                  <button
-                    type="button"
-                    className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none"
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      savePin(_id);
-                    }}
-                  >
-                    Save
-                  </button>
-                )}
-              </div>
-              <div className="flex justify-between items-center gap-2 w-full">
-                {destination && (
-                  <a
-                    href={destination}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="bg-white flex items-center gap-2 text-black font-bold p-2 pl-4 pr-4 rounded-full opacity-70 hover:100 hover:shadow-md"
-                  >
-                    <BsFillArrowUpRightCircleFill />
-                    {destination.length > 15
-                      ? `${destination.slice(0, 15)}...`
-                      : destination}
-                  </a>
-                )}
+              <div className="flex justify-end items-center gap-2 w-full">
                 {postedBy?._id === userInfo?.sub && (
-                  <button
-                    type="button"
-                    className="bg-whiteopacity-70 hover:opacity-100 text-dark font-bold p-2 text-base rounded-3xl hover:shadow-md outline-none"
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      deletePin(_id);
-                    }}
-                  >
-                    <AiTwotoneDelete />
-                  </button>
+                    <button
+                      type="button"
+                      className="bg-white opacity-70 hover:opacity-100 text-dark font-bold p-2 text-base rounded-3xl hover:shadow-md outline-none"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        deletePin(_id);
+                      }}
+                    >
+                      <AiTwotoneDelete />
+                    </button>
                 )}
               </div>
             </div>
@@ -190,9 +146,7 @@ function Pin({
             <p className="font-bold text-black">
               {commentsCount ? commentsCount : "0"}
             </p>
-            <Link
-              to={`/pin-detail/${_id}`}
-            >
+            <Link to={`/pin-detail/${_id}`}>
               <BiMessageRounded className=" text-black text-xl md:text-2xl" />
             </Link>
           </div>
